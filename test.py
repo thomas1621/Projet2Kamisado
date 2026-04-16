1a1f966f971976a4f467799cc1b4f2ca7af90115
Bertholet Thomas <24068@ecam.be>
2026-04-16 15:32:14 +0200

    Connexion Serveur : ping pong réussi.

--- test.py
+++ test.py
@@ -5,23 +5,49 @@ import json
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("172.17.10.46", 3000))
 
-msg = {
+def recv_message(sock):
+    size_data = sock.recv(4)
+    if not size_data:
+        return None
+    size = struct.unpack("I", size_data)[0]
+    chunks = []
+    received = 0
+    while received < size:
+        data = sock.recv(1024)
+        chunks.append(data)
+        received += len(data)
+    full_data = b''.join(chunks)
+    return json.loads(full_data.decode("utf-8"))
+
+def send_message(sock,msg):
+    msg_bytes = json.dumps(msg).encode("utf-8")
+    sock.sendall(struct.pack("I", len(msg_bytes)))
+    sock.sendall(msg_bytes)
+
+# subscribe
+send_message(s,{
     "request": "subscribe",
     "port": 8888,
     "name": "Thomas",
     "matricules": ["24068", "24104"]
-}
+})
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", 8888))
+server.listen()
 
-msg_bytes = json.dumps(msg).encode()
 
-s.sendall(struct.pack("I", len(msg_bytes)))
-s.sendall(msg_bytes)
+while True:
+    client, addr = server.accept()
+    message = recv_message(client)
 
-size_data = s.recv(4)
-size = struct.unpack("I", size_data)[0]
+    if message is None:
+        break
 
-json_data = s.recv(size)
-response = json.loads(json_data.decode("utf-8"))
+    print(message)
 
-print(response)
+    if message.get("request") == "ping":
+        send_message(client,{
+            "response": "pong"
+        })
+    client.close()
 s.close()
